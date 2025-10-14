db8c499e288c643017e7ea51afca95fa3344012e
Huanli210 <jonny20040210@gmail.com>
2025-10-14 16:41:57 +0800

    chinese

--- aimlab_start.py
+++ aimlab_start.py
@@ -2,6 +2,6 @@ import logging
 import aimlab_debug
 import config
 
-logging.basicConfig(level=getattr(logging, config.log_level))  # 设置日志级别
+logging.basicConfig(level=getattr(logging, config.log_level))  # 設定日誌級別
 
 aimlab_debug.aimlab_debug()
